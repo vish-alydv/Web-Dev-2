80ef2cdc36a3f08b57dfd5733ebf276ae0824b6b
vish-alydv <thevishaleditor@gmail.com>
2026-01-19 13:35:50 +0530

    3

---
+++ Js-Day--3/script.js
@@ -0,0 +1,35 @@
+//  let fruits = ["apple", "banana"];
+//  let newLength = fruits.push("cherry"); 
+
+//  console.log(fruits)
+
+
+// let fruits = ["apple", "banana", "cherry"];
+// let removed = fruits.pop();
+
+// console.log(fruits)
+
+
+
+// let fruits = ["apple", "banana", "cherry"];
+// let removed = fruits.shift();
+
+
+
+// let fruits = ["banana", "cherry"];
+// let newLength = fruits.unshift("apple"); 
+
+
+
+let fruits =["apple", "banana", "cherry"];
+let result =[];
+
+
+
+while (fruits.length > 0) {
+    result.push(fruits.pop())
+    
+}
+
+console.log(result)
+
