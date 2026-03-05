5a214f49b55efe91ffdef30b04940e158875cc2b
vish-alydv <thevishaleditor@gmail.com>
2026-03-05 10:51:27 +0530

    Back Again

---
+++ Js-Day--14/script.js
@@ -0,0 +1,53 @@
+
+//  Promises : used to manage asynchronous code effective better than callback HtmlElement And advantage are 
+//  easy to manage the getComputedStyle , easy to manage the codes , easy to debugger , code rediability is good.
+ 
+
+
+
+// function pro() {
+//     return new Promise ((res,rej)=>{
+//         let a=10;
+//         if(a>50){
+//             res();
+//         }
+//         else {
+//             rej()
+//         }
+//     })
+// }
+
+// pro()
+// .then(()=> console.log("Promise Resolved"))
+// .catch(()=> console.log("Promise Rejected"))
+// .finally(()=> console.log("Inside Finally"))
+
+
+
+// Example 1
+// console.log("a");
+// setTimeout(()=>{
+//     console.log("b")
+// },5000);
+// setTimeout(()=>{
+//     console.log("c")
+// },3000);
+// setTimeout(()=>{
+//     console.log("d")
+// },2000);
+
+// console.log("e")
+
+
+
+
+// Example 2
+console.log("a");
+setTimeout(()=>{
+    console.log("b")
+},0);
+Promise.resolve().then(() => {
+    console.log("Promise");
+});
+
+console.log("e")
